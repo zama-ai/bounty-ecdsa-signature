7662461615ac553e2c43cac19229b22574f64b17
Pakorn Nathong <pakorn.nth@hotmail.com>
2023-07-03 01:41:38 +0700

    use naive mul mod

--- src/main.rs
+++ src/main.rs
@@ -11,40 +11,39 @@ use tfhe::{
 
 use crate::{
     helper::format,
-    ops::{add_mod, mul_mod},
+    ops::{add_mod, mul_mod, mul_mod_naive},
 };
 
 pub mod helper;
 pub mod ops;
 
 fn main() {
-    const NUM_BLOCK: usize = 9;
-    type Integer = u16;
-    //const NUM_BLOCK: usize = 129;
-    //type Integer = U256;
-
     let (client_key, server_key) = IntegerKeyCache.get_from_params(PARAM_MESSAGE_2_CARRY_2);
 
-    let p: Integer = 13841;
-    let msg1: Integer = 13820;
+    //const NUM_BLOCK: usize = 8;
+    //type Integer = u16;
+    //let p: Integer = 13841;
+    //let msg1: Integer = 13820;
 
-    //let p = {
-    //let mut p = U256::ZERO;
-    //p.copy_from_be_byte_slice(
-    //&hex::decode("fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f")
-    //.unwrap(),
-    //);
-    //p
-    //};
-    //let msg1 = {
-    //let mut msg = U256::ZERO;
-    ////msg.copy_from_le_byte_slice(&thread_rng().gen::<[u8; 32]>());
-    //msg.copy_from_le_byte_slice(&[
-    //240, 212, 41, 181, 222, 225, 129, 5, 46, 208, 244, 180, 180, 209, 208, 121, 113, 68,
-    //162, 213, 221, 208, 27, 54, 57, 126, 74, 79, 45, 245, 192, 151,
-    //]);
-    //msg % p
-    //};
+    const NUM_BLOCK: usize = 128;
+    type Integer = U256;
+    let p = {
+        let mut p = U256::ZERO;
+        p.copy_from_be_byte_slice(
+            &hex::decode("fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f")
+                .unwrap(),
+        );
+        p
+    };
+    let msg1 = {
+        let mut msg = U256::ZERO;
+        //msg.copy_from_le_byte_slice(&thread_rng().gen::<[u8; 32]>());
+        msg.copy_from_le_byte_slice(&[
+            240, 212, 41, 181, 222, 225, 129, 5, 46, 208, 244, 180, 180, 209, 208, 121, 113, 68,
+            162, 213, 221, 208, 27, 54, 57, 126, 74, 79, 45, 245, 192, 151,
+        ]);
+        msg % p
+    };
 
     let ct_1 = client_key.encrypt_radix(msg1, NUM_BLOCK);
     assert_eq!(msg1, client_key.decrypt_radix::<Integer>(&ct_1));
@@ -63,7 +62,7 @@ fn main() {
     //println!("in {} s\n", elasped.as_secs());
 
     let now = Instant::now();
-    let res = mul_mod::<NUM_BLOCK, _>(&ct_1, &ct_1, p, &server_key);
+    let res = mul_mod_naive::<NUM_BLOCK, _>(&ct_1, &ct_1, p, &server_key);
     let elasped = now.elapsed();
     let res = client_key.decrypt_radix::<Integer>(&res);
     print!(
--- src/ops.rs
+++ src/ops.rs
@@ -12,13 +12,14 @@ pub fn add_mod<const NB: usize, P: DecomposableInto<u64> + DecomposableInto<u8> 
     server_key: &ServerKey,
 ) -> RadixCiphertext {
     // assume large p and a,b < p
-    let mut added = server_key.smart_add_parallelized(&mut a.clone(), &mut b.clone());
-    let mut is_gt = server_key.smart_scalar_gt_parallelized(&mut added, p);
+    let mut a_expanded = server_key.extend_radix_with_trivial_zero_blocks_msb(&a, 1);
+    server_key.smart_add_assign_parallelized(&mut a_expanded, &mut b.clone());
+    let mut is_gt = server_key.smart_scalar_gt_parallelized(&mut a_expanded, p);
     server_key.trim_radix_blocks_msb_assign(&mut is_gt, NB - 1);
     let mut to_sub =
         server_key.smart_mul_parallelized(&mut server_key.create_trivial_radix(p, NB), &mut is_gt);
 
-    server_key.smart_sub_parallelized(&mut added, &mut to_sub)
+    server_key.smart_sub_parallelized(&mut a_expanded, &mut to_sub)
 }
 
 pub fn mul_mod<const NB: usize, P: DecomposableInto<u64> + DecomposableInto<u8> + Copy + Sync>(
@@ -74,4 +75,27 @@ pub fn mul_mod<const NB: usize, P: DecomposableInto<u64> + DecomposableInto<u8> 
     }
 
     add_mod::<NB, _>(&res, &to_add_later, p, server_key)
+}
+
+pub fn mul_mod_naive<
+    const NB: usize,
+    P: DecomposableInto<u64> + DecomposableInto<u8> + Copy + Sync,
+>(
+    a: &RadixCiphertext,
+    b: &RadixCiphertext,
+    p: P,
+    server_key: &ServerKey,
+) -> RadixCiphertext {
+    // assume large p and a,b < p
+    let now = Instant::now();
+    let mut a_expanded = server_key.extend_radix_with_trivial_zero_blocks_msb(&a, NB);
+    println!("expanded a - {}s", now.elapsed().as_secs());
+    server_key.smart_mul_assign_parallelized(&mut a_expanded, &mut b.clone());
+    println!("mul by b - {}s", now.elapsed().as_secs());
+    let (_q, r) = server_key.smart_div_rem_parallelized(
+        &mut a_expanded,
+        &mut server_key.create_trivial_radix(p, NB * 2),
+    );
+    println!("div by p - {}s", now.elapsed().as_secs());
+    server_key.trim_radix_blocks_msb(&r, NB)
 }
