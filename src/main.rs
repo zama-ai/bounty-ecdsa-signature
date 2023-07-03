34ada7ce562ebb8648834c16de4f916fe05ee6ff
Pakorn Nathong <pakorn.nth@hotmail.com>
2023-07-03 13:46:18 +0700

    add group double op

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use tfhe::{
 
 use crate::{
     helper::format,
-    ops::{add_mod, mul_mod, mul_mod_naive},
+    ops::{add_mod, group_double, mul_mod},
 };
 
 pub mod helper;
@@ -20,33 +20,39 @@ pub mod ops;
 fn main() {
     let (client_key, server_key) = IntegerKeyCache.get_from_params(PARAM_MESSAGE_2_CARRY_2);
 
-    //const NUM_BLOCK: usize = 8;
-    //type Integer = u16;
-    //let p: Integer = 13841;
-    //let msg1: Integer = 13820;
+    const NUM_BLOCK: usize = 8;
+    type Integer = u16;
+    let p: Integer = 13841;
+    let x: Integer = 13820;
+    let y: Integer = 388;
+    let z: Integer = 1;
 
-    const NUM_BLOCK: usize = 128;
-    type Integer = U256;
-    let p = {
-        let mut p = U256::ZERO;
-        p.copy_from_be_byte_slice(
-            &hex::decode("fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f")
-                .unwrap(),
-        );
-        p
-    };
-    let msg1 = {
-        let mut msg = U256::ZERO;
-        //msg.copy_from_le_byte_slice(&thread_rng().gen::<[u8; 32]>());
-        msg.copy_from_le_byte_slice(&[
-            240, 212, 41, 181, 222, 225, 129, 5, 46, 208, 244, 180, 180, 209, 208, 121, 113, 68,
-            162, 213, 221, 208, 27, 54, 57, 126, 74, 79, 45, 245, 192, 151,
-        ]);
-        msg % p
-    };
+    //const NUM_BLOCK: usize = 128;
+    //type Integer = U256;
+    //let p = {
+    //let mut p = U256::ZERO;
+    //p.copy_from_be_byte_slice(
+    //&hex::decode("fffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f")
+    //.unwrap(),
+    //);
+    //p
+    //};
+    //let msg1 = {
+    //let mut msg = U256::ZERO;
+    ////msg.copy_from_le_byte_slice(&thread_rng().gen::<[u8; 32]>());
+    //msg.copy_from_le_byte_slice(&[
+    //240, 212, 41, 181, 222, 225, 129, 5, 46, 208, 244, 180, 180, 209, 208, 121, 113, 68,
+    //162, 213, 221, 208, 27, 54, 57, 126, 74, 79, 45, 245, 192, 151,
+    //]);
+    //msg % p
+    //};
 
-    let ct_1 = client_key.encrypt_radix(msg1, NUM_BLOCK);
-    assert_eq!(msg1, client_key.decrypt_radix::<Integer>(&ct_1));
+    let ct_x = client_key.encrypt_radix(x, NUM_BLOCK);
+    let ct_y = client_key.encrypt_radix(y, NUM_BLOCK);
+    let ct_z = client_key.encrypt_radix(z, NUM_BLOCK);
+    assert_eq!(x, client_key.decrypt_radix::<Integer>(&ct_x));
+    assert_eq!(y, client_key.decrypt_radix::<Integer>(&ct_y));
+    assert_eq!(z, client_key.decrypt_radix::<Integer>(&ct_z));
 
     //let now = Instant::now();
     //let res = add_mod::<NUM_BLOCK, _>(&ct_1, &ct_1, p, &server_key);
@@ -61,16 +67,30 @@ fn main() {
     //);
     //println!("in {} s\n", elasped.as_secs());
 
+    //let now = Instant::now();
+    //let res = mul_mod::<NUM_BLOCK, _>(&ct_1, &ct_1, p, &server_key);
+    //let elasped = now.elapsed();
+    //let res = client_key.decrypt_radix::<Integer>(&res);
+    //print!(
+    //"{} * {} mod {} -> {}",
+    //format(msg1),
+    //format(msg1),
+    //format(p),
+    //format(res)
+    //);
+    //println!("in {} s\n", elasped.as_secs());
+
     let now = Instant::now();
-    let res = mul_mod_naive::<NUM_BLOCK, _>(&ct_1, &ct_1, p, &server_key);
+    let (x_new, y_new, z_new) = group_double::<NUM_BLOCK, _>(&ct_x, &ct_y, &ct_z, p, &server_key);
     let elasped = now.elapsed();
-    let res = client_key.decrypt_radix::<Integer>(&res);
     print!(
-        "{} * {} mod {} -> {}",
-        format(msg1),
-        format(msg1),
-        format(p),
-        format(res)
+        "{},{},{} * 2 -> {},{},{}",
+        format(x),
+        format(y),
+        format(z),
+        format(client_key.decrypt_radix::<Integer>(&x_new)),
+        format(client_key.decrypt_radix::<Integer>(&y_new)),
+        format(client_key.decrypt_radix::<Integer>(&z_new))
     );
     println!("in {} s\n", elasped.as_secs());
 }
--- src/ops.rs
+++ src/ops.rs
@@ -1,10 +1,11 @@
 use std::time::Instant;
 
 use tfhe::{
-    core_crypto::prelude::Numeric,
+    core_crypto::prelude::{Numeric, UnsignedInteger},
     integer::{block_decomposition::DecomposableInto, RadixCiphertext, ServerKey},
 };
 
+/// a + b mod p
 pub fn add_mod<const NB: usize, P: DecomposableInto<u64> + DecomposableInto<u8> + Copy + Sync>(
     a: &RadixCiphertext,
     b: &RadixCiphertext,
@@ -18,11 +19,34 @@ pub fn add_mod<const NB: usize, P: DecomposableInto<u64> + DecomposableInto<u8> 
     server_key.trim_radix_blocks_msb_assign(&mut is_gt, NB - 1);
     let mut to_sub =
         server_key.smart_mul_parallelized(&mut server_key.create_trivial_radix(p, NB), &mut is_gt);
-
-    server_key.smart_sub_parallelized(&mut a_expanded, &mut to_sub)
+    server_key.smart_sub_assign_parallelized(&mut a_expanded, &mut to_sub);
+    server_key.trim_radix_blocks_msb_assign(&mut a_expanded, 1);
+    a_expanded
 }
 
-pub fn mul_mod<const NB: usize, P: DecomposableInto<u64> + DecomposableInto<u8> + Copy + Sync>(
+/// a - b mod p
+pub fn sub_mod<const NB: usize, P: DecomposableInto<u64> + DecomposableInto<u8> + Copy + Sync>(
+    a: &RadixCiphertext,
+    b: &RadixCiphertext,
+    p: P,
+    server_key: &ServerKey,
+) -> RadixCiphertext {
+    let mut is_gt = server_key.smart_gt_parallelized(&mut b.clone(), &mut a.clone());
+    let mut to_add =
+        server_key.smart_mul_parallelized(&mut server_key.create_trivial_radix(p, NB), &mut is_gt);
+    let mut a_expanded = server_key.extend_radix_with_trivial_zero_blocks_msb(&a, 1);
+    server_key.trim_radix_blocks_msb_assign(&mut is_gt, NB - 1);
+    server_key.smart_add_assign_parallelized(&mut a_expanded, &mut to_add);
+    server_key.smart_sub_assign_parallelized(&mut a_expanded, &mut b.clone());
+    server_key.trim_radix_blocks_msb_assign(&mut a_expanded, 1);
+    a_expanded
+}
+
+/// a * b mod p
+pub fn mul_mod_bitwise<
+    const NB: usize,
+    P: DecomposableInto<u64> + DecomposableInto<u8> + Copy + Sync,
+>(
     a: &RadixCiphertext,
     b: &RadixCiphertext,
     p: P,
@@ -77,25 +101,171 @@ pub fn mul_mod<const NB: usize, P: DecomposableInto<u64> + DecomposableInto<u8> 
     add_mod::<NB, _>(&res, &to_add_later, p, server_key)
 }
 
-pub fn mul_mod_naive<
-    const NB: usize,
-    P: DecomposableInto<u64> + DecomposableInto<u8> + Copy + Sync,
->(
+/// a * b mod p
+pub fn mul_mod<const NB: usize, P: DecomposableInto<u64> + DecomposableInto<u8> + Copy + Sync>(
     a: &RadixCiphertext,
     b: &RadixCiphertext,
     p: P,
     server_key: &ServerKey,
 ) -> RadixCiphertext {
     // assume large p and a,b < p
-    let now = Instant::now();
     let mut a_expanded = server_key.extend_radix_with_trivial_zero_blocks_msb(&a, NB);
-    println!("expanded a - {}s", now.elapsed().as_secs());
     server_key.smart_mul_assign_parallelized(&mut a_expanded, &mut b.clone());
-    println!("mul by b - {}s", now.elapsed().as_secs());
-    let (_q, r) = server_key.smart_div_rem_parallelized(
+    let (_q, mut r) = server_key.smart_div_rem_parallelized(
         &mut a_expanded,
         &mut server_key.create_trivial_radix(p, NB * 2),
     );
-    println!("div by p - {}s", now.elapsed().as_secs());
-    server_key.trim_radix_blocks_msb(&r, NB)
+    server_key.trim_radix_blocks_msb_assign(&mut r, NB);
+    r
+}
+
+/// a * b mod p where b is a constant
+/// slower than 12 `add_mod`
+pub fn mul_mod_constant<
+    const NB: usize,
+    P: DecomposableInto<u64> + DecomposableInto<u8> + Copy + Sync,
+    S: DecomposableInto<u8> + UnsignedInteger,
+>(
+    a: &RadixCiphertext,
+    b: S,
+    p: P,
+    server_key: &ServerKey,
+) -> RadixCiphertext {
+    // assume large p and a,b < p
+    let mut a_expanded = server_key.extend_radix_with_trivial_zero_blocks_msb(&a, NB);
+    server_key.smart_scalar_mul_assign_parallelized(&mut a_expanded, b);
+    let (_q, mut r) = server_key.smart_div_rem_parallelized(
+        &mut a_expanded,
+        &mut server_key.create_trivial_radix(p, NB * 2),
+    );
+    server_key.trim_radix_blocks_msb_assign(&mut r, NB);
+    r
+}
+
+/// a^2 mod p
+pub fn square_mod<
+    const NB: usize,
+    P: DecomposableInto<u64> + DecomposableInto<u8> + Copy + Sync,
+>(
+    a: &RadixCiphertext,
+    p: P,
+    server_key: &ServerKey,
+) -> RadixCiphertext {
+    mul_mod::<NB, _>(a, a, p, server_key)
+}
+
+/// a*2 mod p
+pub fn double_mod<
+    const NB: usize,
+    P: DecomposableInto<u64> + DecomposableInto<u8> + Copy + Sync,
+>(
+    a: &RadixCiphertext,
+    p: P,
+    server_key: &ServerKey,
+) -> RadixCiphertext {
+    add_mod::<NB, _>(a, a, p, server_key)
+}
+
+pub fn group_double<
+    const NB: usize,
+    P: DecomposableInto<u64> + DecomposableInto<u8> + Copy + Sync,
+>(
+    x: &RadixCiphertext,
+    y: &RadixCiphertext,
+    z: &RadixCiphertext,
+    p: P,
+    server_key: &ServerKey,
+) -> (RadixCiphertext, RadixCiphertext, RadixCiphertext) {
+    let timer = Instant::now();
+    // y^2 = x^3+ax+b
+    // in case a == 0
+
+    // a = x^2
+    // b = y^2
+    let (a, b) = rayon::join(
+        || square_mod::<NB, _>(x, p, server_key),
+        || square_mod::<NB, _>(y, p, server_key),
+    );
+    println!("a,b - {:.2}s", timer.elapsed().as_secs_f64());
+    // c = b^2
+    let c = square_mod::<NB, _>(&b, p, server_key);
+    println!("c - {:.2}s", timer.elapsed().as_secs_f64());
+    // d = 2(2(x + b) - (a + c))
+    let (xb2, ac) = rayon::join(
+        || double_mod::<NB, _>(&add_mod::<NB, _>(x, &b, p, server_key), p, server_key),
+        || add_mod::<NB, _>(&a, &c, p, server_key),
+    );
+    let d = double_mod::<NB, _>(
+        &sub_mod::<NB, _>(
+            &xb2, // 2(x + b)
+            &ac,  // (a + c)
+            p, server_key,
+        ),
+        p,
+        server_key,
+    );
+    println!("d - {:.2}s", timer.elapsed().as_secs_f64());
+    // e = 3a
+    let e = add_mod::<NB, _>(&double_mod::<NB, _>(&a, p, server_key), &a, p, server_key);
+    println!("e - {:.2}s", timer.elapsed().as_secs_f64());
+    // f = e^2
+    let f = square_mod::<NB, _>(&e, p, server_key);
+    println!("f - {:.2}s", timer.elapsed().as_secs_f64());
+    // z' = 2*y1*z1
+    // x' = f - 2d
+    let (z_prime, x_prime) = rayon::join(
+        || double_mod::<NB, _>(&mul_mod::<NB, _>(y, z, p, server_key), p, server_key),
+        || sub_mod::<NB, _>(&f, &double_mod::<NB, _>(&d, p, server_key), p, server_key),
+    );
+    println!("z',x' - {:.2}s", timer.elapsed().as_secs_f64());
+    // y' = e(d - x') - 8c
+    let (edx, c8) = rayon::join(
+        || {
+            mul_mod::<NB, _>(
+                &e,
+                &sub_mod::<NB, _>(&d, &x_prime, p, server_key),
+                p,
+                server_key,
+            )
+        },
+        || {
+            let enc_2c = double_mod::<NB, _>(&c, p, &server_key);
+            let enc_4c = double_mod::<NB, _>(&enc_2c, p, &server_key);
+            double_mod::<NB, _>(&enc_4c, p, &server_key)
+        },
+    );
+    let y_prime = sub_mod::<NB, _>(&edx, &c8, p, server_key);
+    println!("y' - {:.2}s", timer.elapsed().as_secs_f64());
+
+    (x_prime, y_prime, z_prime)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::Instant;
+
+    use tfhe::{integer::keycache::IntegerKeyCache, shortint::prelude::PARAM_MESSAGE_2_CARRY_2};
+
+    use crate::ops::{double_mod, mul_mod_constant};
+
+    #[test]
+    #[ignore = "bench"]
+    fn bench_8a() {
+        let (client_key, server_key) = IntegerKeyCache.get_from_params(PARAM_MESSAGE_2_CARRY_2);
+        const NUM_BLOCK: usize = 8;
+        type Integer = u16;
+        let p: Integer = 13841;
+        let a: Integer = 13820;
+        let enc_a = client_key.encrypt_radix(a, NUM_BLOCK);
+
+        let timer = Instant::now();
+        let enc_2a = double_mod::<NUM_BLOCK, _>(&enc_a, p, &server_key);
+        let enc_4a = double_mod::<NUM_BLOCK, _>(&enc_2a, p, &server_key);
+        let _enc_8a = double_mod::<NUM_BLOCK, _>(&enc_4a, p, &server_key);
+        println!("8a using addition - {}s", timer.elapsed().as_secs());
+
+        let timer = Instant::now();
+        let _enc_8a_mul = mul_mod_constant::<NUM_BLOCK, _, _>(&enc_a, 8u8, p, &server_key);
+        println!("8a using multiplication - {}s", timer.elapsed().as_secs());
+    }
 }
