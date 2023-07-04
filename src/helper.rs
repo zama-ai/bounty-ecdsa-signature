f70608efe15b077b6fb6623f5baf2bafdae8d276
Pakorn Nathong <pakorn.nth@hotmail.com>
2023-07-05 00:32:02 +0700

    add u256 from string

--- src/helper.rs
+++ src/helper.rs
@@ -13,3 +13,26 @@ pub fn format<T: DecomposableInto<u8> + Copy>(a: T) -> String {
     )
     .to_string()
 }
+
+pub fn u256_from_decimal_string(s: &str) -> U256 {
+    let mut res = U256::ZERO;
+    for c in s.chars() {
+        res *= 10u8.into();
+        res += (c as u8 - b'0').into();
+    }
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::helper::format;
+
+    use super::u256_from_decimal_string;
+
+    #[test]
+    fn correct_u256_from_string() {
+        let str_value = "47328345983454384985382623486293476776767";
+        let u256_value = u256_from_decimal_string(str_value);
+        assert_eq!(format(u256_value), str_value);
+    }
+}
