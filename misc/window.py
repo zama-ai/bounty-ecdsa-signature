d15107c9b9bce0a62b8309d7d6623cc81ba081fe
yoyoismee <yoyoismee@gmail.com>
2023-09-21 18:07:52 +0700

    update plot

--- misc/window.py
+++ misc/window.py
@@ -15,7 +15,16 @@ for i in range(1,12):
 
 plt.figure(figsize=(8,6))
 plt.plot(range(1,12), result)
-plt.xlabel('window size')
-plt.ylabel('estimated time')
-plt.title('Estimated time for different window size')
+# add annotation on the plot
+for i in range(4,7):
+    plt.annotate('(%d, %d)' % (i, result[i-1]), xy=(i, result[i-1]), xytext=(i, result[i-1]+5000))
+i = 7
+plt.annotate('(%d, %d)' % (i, result[i-1]), xy=(i, result[i-1]), xytext=(i, result[i-1]+15000))
+i = 8
+plt.annotate('(%d, %d)' % (i, result[i-1]), xy=(i, result[i-1]), xytext=(i, result[i-1]+20000))
+
+
+plt.xlabel('window size (bits)')
+plt.ylabel('estimated time (seconds)')
+plt.title('Estimated time vs window size for 256-bit on n2d-standard-64')
 plt.savefig("window.png")
